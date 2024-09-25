1592b48538e265e20029a8c9f96acc4e386eab4f
Christian Rafael Guado Geronimo <crguado@gmail.com>
2024-09-25 15:36:02 -0500

    GET: parámetros query

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { faker } from '@faker-js/faker';
 
 const app = express();
 const port = 3000;
@@ -12,11 +13,24 @@ app.get("/nueva-ruta", (req, res) => {
 });
 
 app.get("/products", (req, res) => {
-  res.json(
-    { name: "Product 1", price: 1000 },
-    { name: "Product 2", price: 2000 }
-  );
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+
+  for (let i = 0; i < limit; i++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.url(),
+    });
+  }
+
+  res.json(products);
 });
+
+app.get('/products/filter', (req, res) => {
+  res.send('Soy un filtro')
+})
 
 app.get("/products/:id", (req, res) => {
   const { id } = req.params;
@@ -25,6 +39,19 @@ app.get("/products/:id", (req, res) => {
     name: "Product 2",
     price: 2000,
   });
+});
+
+app.get("/users", (req, res) => {
+  const { limit, offset } = req.query;
+
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset,
+    });
+  } else {
+    res.send("No hay parámetros");
+  }
 });
 
 app.get("/categories/:categoryId/products/:productId", (req, res) => {
